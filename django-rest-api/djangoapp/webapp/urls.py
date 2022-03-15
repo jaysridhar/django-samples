32931283d4b1d11834f6d5ff553532b614a3070e
Jay Sridhar <jay.sridhar@gmail.com>
2022-03-15 18:58:18 +0530

    Added a view to retrieve something via API.

--- django-rest-api/djangoapp/webapp/urls.py
+++ django-rest-api/djangoapp/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from webapp import views
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.index),
-    path('self', views.self_view),
+    path('self', csrf_exempt(views.SelfView.as_view())),
 ]
--- django-rest-api/djangoapp/webapp/views.py
+++ django-rest-api/djangoapp/webapp/views.py
@@ -3,9 +3,19 @@ from django.shortcuts import render
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.views import APIView
 
 def index(request):
     return render(request, 'base_home.html', {'user': request.user})
+
+class SelfView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        content = {
+            'user': str(request.user),  # `django.contrib.auth.User` instance.
+            'auth': str(request.auth),  # None
+        }
+        return JsonResponse(content, safe=False)
 
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
