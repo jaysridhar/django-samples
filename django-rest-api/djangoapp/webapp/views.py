997ce88889321095ad096fd3f4d6e356c9f4598c
Jay Sridhar <jay.sridhar@gmail.com>
2022-03-15 15:39:30 +0530

    Showing login screen for the home page.

--- django-rest-api/djangoapp/webapp/views.py
+++ django-rest-api/djangoapp/webapp/views.py
@@ -1,5 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 
-def index(response):
-    return HttpResponse('Hello World')
+def index(request):
+    return render(request, 'base_home.html', {'user': request.user})
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def self_view(request, format=None):
+    content = {
+        'user': str(request.user),  # `django.contrib.auth.User` instance.
+        'auth': str(request.auth),  # None
+    }
+    return JsonResponse(content, safe=False)
